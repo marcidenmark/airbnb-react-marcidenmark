4e4c36d1e2ec343dbc8012ede47112d881e27807
marci papineau <marcidenmark@hotmail.com>
2018-01-11 13:06:07 +0100

    added google map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from './components/flat';
 
-class App extends Component {
-  render() {
-   const flat ={
-    "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
-    "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.346890,
-  };
 
-    const flats = [ flat, flat, flat, flat, flat ];
+class App extends Component {
+ constructor(props){
+   super(props);
+   this.state = {
+    flats: []
+   };
+ }
+ componentDidMount () {
+  const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+  fetch(url) //ajax
+   .then(response => response.json( ))
+   .then((data) => {
+    this.setState({
+     flats:data
+    })
+   })
+ }
+
+
+
+
+  render() {
+     const center = {
+   lat: 48.8566,
+   lng: 2.3522
+  }
+
 
     return (
      <div className="app">
@@ -21,13 +38,19 @@ class App extends Component {
         <div className="search">
         </div>
        <div className="flats">
-        {flats.map(function(flat){
+        {this.state.flats.map((flat) => {
          return <Flat flat={flat} />
           })}
 
        </div>
       </div>
        <div className="map">
+       <GoogleMapReact
+        Center={center}
+        Zoom={11}
+      >
+      </GoogleMapReact>
+
        </div>
      </div>
     );
