1b088d1f9d1d48b279af0b23fa18c05b37711da6
marci papineau <marcidenmark@hotmail.com>
2018-01-11 11:50:24 +0100

    the grid has been fixed in flatJs file

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
   render() {
    const flat ={
     "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
+    // "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+    // "price": 164,
     "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.346890
+    // "lat": 48.884211,
+    // "lng": 2.346890,
   };
 
     return (
@@ -18,7 +18,7 @@ class App extends Component {
        <div className="main">
         <div className="search">
         </div>
-       <div className="flats" >
+       <div className="flats">
         <Flat flat={flat} />
         <Flat flat={flat} />
         <Flat flat={flat} />
--- src/components/flat.js
+++ src/components/flat.js
@@ -1,33 +1,20 @@
 import React from "react";
 import "./flat.css";
 
+
+
 class Flat extends React.Component {
- render(){
-  const title =
-   this.props.flat.name +
-   + " - " +
-   this.props.flat.imageUrl +
-   this.props.flat.price +
-   this.props.flat.priceCurrency +
-   this.props.flat.lat +
-   this.props.flat.lng;
-
-  const style = {
-   backgroundImage: "url('" + this.props.flat.imageUrl + "')"
-   // //instead let's use template literals from es6!
-   // backgroundImage: `url('$this.props.flat.imageUrl}')`
-
-  };
-
-  return (
-   <div className= "flat">
-    <div className="flat-picture"> </div>
-    <div className="flat-title">
-     {title}
-    </div>
-   </div>
-   );
+  render() {
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    };
+    const title = `${this.props.flat.price}€ - ${this.props.flat.name}`
+    return (
+      <div className="flat" onClick={this.handleClick}>
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">{title}</div>
+      </div>);
+  }
  }
-}
 
 export default Flat;
